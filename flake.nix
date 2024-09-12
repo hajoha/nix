aa0333b11d025f5db7f368ebc102ad28ec0a5340
hajoha <hajoha1@proton.me>
2024-09-12 23:03:48 +0200

    clean up

--- flake.nix
+++ flake.nix
@@ -8,34 +8,32 @@
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-
    nur.url = "github:nix-community/nur";
   };
 
 
   outputs = { self, nixpkgs, home-manager,... }@inputs: 
-{	# Please replace my-nixos with your hostname
-    nixosConfigurations = {
-nixmaschine = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-#            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
+    {	# Please replace my-nixos with your hostname
+        nixosConfigurations = {
+        nixmaschine = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            # Import the previous configuration.nix we used,
+            # so the old configuration file still takes effect
+            ./configuration.nix
+              home-manager.nixosModules.home-manager
+              {
+    #            home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
 
-            home-manager.users.hajoha = import ./home.nix;
+                home-manager.users.hajoha = import ./home.nix;
 
-            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
-            home-manager.extraSpecialArgs = { inherit inputs; };
+                # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+                home-manager.extraSpecialArgs = { inherit inputs; };
 
-          }
-      ];
+              }
+          ];
+        };
     };
-};
   };
 }
