89eb762d1b414dc27c5f3391e19da1b1e613fc24
Johann Hackler <johann.hackler@fokus.fraunhofer.de>
2026-01-12 16:07:08 +0100

    add autossh configuration and update ssh secrets management

--- hosts/nixgrafana/configuration.nix
+++ hosts/nixgrafana/configuration.nix
@@ -21,6 +21,50 @@
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   sops.defaultSopsFile = ./secrets/grafana-creds.enc.yaml;
   networking.firewall.allowedTCPPorts = [ 3000 ];
+  sops.secrets.dsp25-ssh = {
+    sopsFile = ./secrets/ssh-creds.enc.yaml;
+  };
+  services.autossh.sessions = [
+    {
+      extraArguments = "-N -T -F /etc/ssh/ssh_config dsp25-main-influx";
+      monitoringPort = 20000;
+      name = "dsp25-main-influx";
+      user = "root";
+    }
+  ];
+  programs.ssh.extraConfig = "
+Include ${config.sops.secrets.dsp25-ssh.path}
+  ";
+programs.ssh.startAgent = true;
+  networking = {
+    defaultGateway = {
+      address = "10.60.1.1";
+      interface = "service";
+    };
+    interfaces.service = {
+      ipv4 = {
+        addresses = [
+          {
+            address = "10.60.1.25";
+            prefixLength = 24;
+          }
+        ];
+        routes = [
+#          {
+#            address = "141.23.28.221";
+#            prefixLength = 32;
+#            via = "10.60.1.25";
+#          }
+          {
+            address = "10.60.1.0";
+            prefixLength = 24;
+            via = "10.60.1.25";
+          }
+        ];
+      };
+    };
+  };
+
   system.stateVersion = "24.05";
 
 }
