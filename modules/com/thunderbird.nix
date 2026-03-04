cc122e568cc476d930f034b2ba10ec50795b5777
Johann Hackler <johann.hackler@fokus.fraunhofer.de>
2026-03-04 20:08:32 +0100

    update

---
+++ modules/com/thunderbird.nix
@@ -0,0 +1,17 @@
+{ pkgs, config, ... }: {
+programs.thunderbird = {
+    enable = true;
+    #  package = pkgs.thunderbird;
+
+    # Change the profile name from "haa" to your actual old folder name:
+    profiles."lbmpfe0j.default-release" = {
+      isDefault = true;
+      # Ensure this matches the folder name exactly
+
+      settings = {
+        "mail.openpgp.allow_external_gnupg" = true;
+      };
+    };
+  };
+
+}
